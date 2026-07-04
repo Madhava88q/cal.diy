d13c8478a45761aa1afa0f507223805c50b313dd
madhava <maadhava78@gmail.com>
2026-07-04 15:27:30 +0530

    test: retest Area 12.9 GraphQL schema after fix

---
+++ apps/web/lib/qedix-area12/graphql/retest-schema-after-fix.ts
@@ -0,0 +1,22 @@
+﻿export const qedixArea12GraphqlRetestSchemaAfterFix = `
+  type Query {
+    publicProfile(
+      id: ID!
+      locale: String!
+      timezone: String!
+    ): PublicProfile!
+  }
+
+  type PublicProfile {
+    id: ID!
+    displayName: String!
+    locale: String!
+    timezone: String!
+  }
+
+  input PublicProfileLookupInput {
+    id: ID!
+    locale: String!
+    timezone: String!
+  }
+`;
