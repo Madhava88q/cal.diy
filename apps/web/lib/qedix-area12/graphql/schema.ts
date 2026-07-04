f675a86a139d7a5e2b0b75d6b4503bb30e4d5b84
madhava <maadhava78@gmail.com>
2026-07-04 11:39:08 +0530

    test: add Area 12.9 GraphQL schema changed canary

---
+++ apps/web/lib/qedix-area12/graphql/schema.ts
@@ -0,0 +1,19 @@
+﻿export const qedixArea12GraphqlSchema = /* GraphQL */ `
+  type QedixArea12Profile {
+    id: ID!
+    email: String!
+    displayName: String!
+
+    # Qedix Area 12.9 controlled canary:
+    # This intentionally changes a GraphQL public schema contract.
+    # Existing clients may not expect these newly required fields.
+    locale: String!
+    timezone: String!
+  }
+
+  type Query {
+    qedixArea12Profile(id: ID!): QedixArea12Profile!
+  }
+`;
+
+export const qedixArea12GraphqlContractVersion = "area-12-graphql-schema-canary";
