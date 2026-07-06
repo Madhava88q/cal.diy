c77089c8298f5ec3425afd301a6878d520cb7228
madhava <maadhava78@gmail.com>
2026-07-06 11:38:01 +0530

    test: retest Area 13.10 unsafe client redirect

---
+++ apps/web/app/qedix-area13/unsafe-client-redirect/UnsafeClientRedirect.tsx
@@ -0,0 +1,15 @@
+﻿"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+export function QedixUnsafeClientRedirect() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  function continueAfterLogin() {
+    const returnTo = searchParams.get("returnTo") || searchParams.get("callbackUrl") || "/";
+    router.push(returnTo);
+  }
+
+  return <button onClick={continueAfterLogin}>Continue</button>;
+}
