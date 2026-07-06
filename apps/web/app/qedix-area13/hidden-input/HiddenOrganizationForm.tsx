700de0648d7b56f087667ec765d6e56381b4d7fd
madhava <maadhava78@gmail.com>
2026-07-06 11:22:51 +0530

    test: retest Area 13.3 hidden input trusted

---
+++ apps/web/app/qedix-area13/hidden-input/HiddenOrganizationForm.tsx
@@ -0,0 +1,11 @@
+﻿"use client";
+
+export function QedixHiddenOrganizationForm() {
+  return (
+    <form action="/api/qedix-area13/hidden-org-form" method="post">
+      <input type="hidden" name="organizationId" value="org_from_browser_hidden_field" />
+      <input type="hidden" name="role" value="owner" />
+      <button type="submit">Transfer organization</button>
+    </form>
+  );
+}
